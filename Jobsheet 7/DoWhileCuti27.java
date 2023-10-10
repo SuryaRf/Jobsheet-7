f37fa4d8be6e680bf12386a167074d4b36d0a1d1
SuryaRf <141994348+SuryaRf@users.noreply.github.com>
2023-10-10 17:42:25 +0700

    Add files via upload

---
+++ Jobsheet 7/DoWhileCuti27.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DoWhileCuti27 {
+    public static void main(String[] args) {
+        Scanner sc27 = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc27.nextInt();
+
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc27.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+            
+            System.out.print("Jumlah hari: ");
+            jumlahHari = sc27.nextInt();
+
+            if (jumlahHari <= jatahCuti) {
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti: " + jatahCuti);
+            } else {
+                System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                break;
+            }
+        }
+     } while (jatahCuti > 0);
+    }
+}
