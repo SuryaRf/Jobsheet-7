fd36fd49f97c3a44b3e8b3ef6b88546e5cf7ca4a
SuryaRf <141994348+SuryaRf@users.noreply.github.com>
2023-10-12 18:32:21 +0700

    Add files via upload

---
+++ Jobsheet 7/DeretBil27.java
@@ -0,0 +1,20 @@
+package Jobsheet7_2;
+
+import java.util.Scanner;
+
+public class DeretBil27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan bilangan : ");
+        int bil = input.nextInt();
+
+        for (int i = 1; i <= bil; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.println("Bilangan yang bukan kelipatan 3 : " + i);
+        }
+
+    }
+}
