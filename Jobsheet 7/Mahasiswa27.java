67574b01b7db2bd7cb9a99339fdc639d48e49ff5
SuryaRf <141994348+SuryaRf@users.noreply.github.com>
2023-10-12 18:28:20 +0700

    Add files via upload

---
+++ Jobsheet 7/Mahasiswa27.java
@@ -0,0 +1,27 @@
+package Jobsheet7_2;
+import java.util.Scanner;
+
+public class Mahasiswa27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama, jenisKelamin;
+        int count = 0;
+        String output = "";
+        
+        while (count < 10 ) {
+            System.out.print("Masukkan nama mahasiswa: ");
+            nama = input.nextLine();
+            System.out.print("Masukkan jenis kelamin mahasiswa (L/P): ");
+            jenisKelamin = input.nextLine();
+            
+            if (jenisKelamin.equalsIgnoreCase("P")) {
+                output += nama + "\n";
+            }
+
+            count++;
+        }
+        
+        System.out.println("Nama-nama mahasiswa perempuan:");
+        System.out.println(output);
+    }
+}
